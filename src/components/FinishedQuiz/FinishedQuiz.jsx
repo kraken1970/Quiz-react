e0126b81d5782e64a2726a9552d1a47cf97a6070
Serg Minin <everestdon@ukr.net>
2018-11-14 01:15:13 +0300

    the button is highlighted in a separate component

--- src/components/FinishedQuiz/FinishedQuiz.jsx
+++ src/components/FinishedQuiz/FinishedQuiz.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import classes from "./FinishedQuiz.css";
+import Button from "../UserInterface/Button/Button";
 
 const FinishedQuiz = props => {
   const succesCount = Object.keys(props.results).reduce((total, key) => {
@@ -31,7 +32,10 @@ const FinishedQuiz = props => {
       </p>
 
       <div>
-        <button onClick={props.onRetry}>Повторить</button>
+        <Button onClick={props.onRetry} type="primary">
+          Повторить
+        </Button>
+        <Button type="success">Перейти в список тестов</Button>
       </div>
     </div>
   );
