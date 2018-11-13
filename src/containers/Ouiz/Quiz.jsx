8cf76d1b709130e02aa6ec116ed3384c889fe37b
Serg Minin <everestdon@ukr.net>
2018-11-14 00:40:11 +0300

    added calculation of results and return to the beginning of tests

--- src/containers/Ouiz/Quiz.jsx
+++ src/containers/Ouiz/Quiz.jsx
@@ -5,7 +5,8 @@ import FinishedQuiz from "../../components/FinishedQuiz/FinishedQuiz";
 
 class Quiz extends Component {
   state = {
-    isFinished: true,
+    isFinished: false,
+    results: {}, //{ [id]: 'success' or 'error'}
     activeQuestion: 0,
     answerState: null, //{ [id]: 'success' or 'error'}
     quiz: [
@@ -42,9 +43,15 @@ class Quiz extends Component {
       }
     }
     const question = this.state.quiz[this.state.activeQuestion];
+    const results = this.state.results;
     if (question.rightAnswerId === answerId) {
+      if (!results[question.id]) {
+        results[question.id] = "success";
+      }
+
       this.setState({
-        answerState: { [answerId]: "success" }
+        answerState: { [answerId]: "success" },
+        results
       });
 
       const timeout = window.setTimeout(() => {
@@ -62,8 +69,10 @@ class Quiz extends Component {
         window.clearTimeout(timeout);
       }, 1000);
     } else {
+      results[question.id] = "error";
       this.setState({
-        answerState: { [answerId]: "error" }
+        answerState: { [answerId]: "error" },
+        results
       });
     }
   };
@@ -72,6 +81,15 @@ class Quiz extends Component {
     return this.state.activeQuestion + 1 === this.state.quiz.length;
   }
 
+  retryHandler = () => {
+    this.setState({
+      activeQuestion: 0,
+      answerState: null,
+      isFinished: false,
+      results: {}
+    });
+  };
+
   render() {
     return (
       <div className={classes.Quiz}>
@@ -79,7 +97,11 @@ class Quiz extends Component {
           <h1>Ответьте на все вопросы:</h1>
 
           {this.state.isFinished ? (
-            <FinishedQuiz />
+            <FinishedQuiz
+              results={this.state.results}
+              quiz={this.state.quiz}
+              onRetry={this.retryHandler}
+            />
           ) : (
             <ActiveQuiz
               answers={this.state.quiz[this.state.activeQuestion].answers}
