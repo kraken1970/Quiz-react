551376a9d1458eda6be89930894a698d0052f738
Serg Minin <everestdon@ukr.net>
2018-11-17 16:00:24 +0300

    setting up a router, creating pages, navigating

--- src/components/Navigation/Drawer/Drawer.jsx
+++ src/components/Navigation/Drawer/Drawer.jsx
@@ -1,15 +1,31 @@
 import React, { Component } from "react";
 import classes from "./Drawer.css";
+import { NavLink } from "react-router-dom";
 import Backdrop from "../../UserInterface/Backdrop/Backdrop";
 
-const links = [1, 2, 3];
+const links = [
+  { to: "/", label: "Список", exact: true },
+  { to: "/auth", label: "Авторизация", exact: false },
+  { to: "/quiz-creator", label: "Создать тест", exact: false }
+];
 
 export default class Drawer extends Component {
+  clickHandler = () => {
+    this.props.onClose();
+  };
+
   renderLinks() {
     return links.map((link, index) => {
       return (
         <li key={index}>
-          <a>Link {link}</a>
+          <NavLink
+            to={link.to}
+            exact={link.exact}
+            activeClassName={classes.active}
+            onClick={this.clickHandler}
+          >
+            {link.label}
+          </NavLink>
         </li>
       );
     });
